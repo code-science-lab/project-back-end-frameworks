47a8a847c1f0ce3fc5aa391c8124fb0c27f68839
AlanLuo\iteam <iteamalan@hotmail.com>
2025-04-14 17:24:51 +0800

    init project - 初始化项目

---
+++ src/main/java/io/codescience/pharos/incubation/PharosIncubationApplication.java
@@ -0,0 +1,13 @@
+package io.codescience.pharos.incubation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharosIncubationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharosIncubationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codescience/pharos/incubation/PharosIncubationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codescience.pharos.incubation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharosIncubationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
